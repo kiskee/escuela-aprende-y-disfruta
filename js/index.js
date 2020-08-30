eab4c7d96a5de7627bf4e4037ff776e78e580f46
kiskee <53805976+kiskee@users.noreply.github.com>
2020-08-29 23:52:34 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,29 +1,74 @@
 // Hemos omitido los acentos en los comentarios por compatibilidad
 
 //Define las variables que necesites
+var fechahoy;
+var eventos;
+var mostrarEventosNuevos = [];
+var mostrarEventosViejos = [];
+var eventosNuevos = [];
+var eventosViejos = [];
 
 
 $(document).ready(function () {
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX
-
-  //Guarda el resultado en variables
-
-  //Clasifica los eventos segun la fecha actual del JSON
-
+$.ajax({
+  url:"info.json"
+}).done(function(resultado){
+//Guarda el resultado en variables
+fechahoy = new Date(resultado.fechahoy);
+eventos = resultado.eventos;
+//Clasifica los eventos segun la fecha actual del JSON
+for(i=0;i<eventos.length;i++){
+  if(new Date(eventos[i].fecha) > fechahoy){
+    eventosNuevos.push(eventos[i]);
+  }else{
+    eventosViejos.push(eventos[i]);
+  }
+}
   //Ordena los eventos segun la fecha (los mas cercanos primero)
+  eventosNuevos = eventosNuevos.sort(function(x,y){
+    if (new Date(x.fecha) > new Date(y.fecha)){
+      return 1;
+    }
+    return -1;
+  });
+//Extrae solo dos eventos
+for(i=0;i<2;i++){
+  mostrarEventosNuevos.push(eventosNuevos[i]);
+}
+ //Crea un string que contenga el HTML que describe el detalle del evento
+ var nuevosHtml = ""
+//Recorre el arreglo y concatena el HTML para cada evento
+for (var j=0;j<mostrarEventosNuevos.length;j++){
+  nuevosHtml += `
+    <div class="col-5 bg-light text-left mx-2 px-5 rounded">
+    <h2><a href="./detalle.html?id=${mostrarNuevosEventos[j].id}">${mostrarNuevosEventos[j].nombre}</a></h2>
+    <p class="text-muted">${mostrarNuevosEventos[j].fecha}</p>
+    <p>${mostrarNuevosEventos[j].descripcion}</p>
+    </div>`
+}
+ //Modifica el DOM agregando el html generado
+document.getElementById("nuevosEventos").innerHTML = nuevosHtml;
 
-  //Extrae solo dos eventos
+})
+  
 
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
+  
 
-  //Extrae solo dos eventos
 
-  //Crea un string que contenga el HTML que describe el detalle del evento
 
-  //Recorre el arreglo y concatena el HTML para cada evento
+  
 
-  //Modifica el DOM agregando el html generado
+  
+
+
+
+ 
+
+  
+
+ 
 
   //Crea un string que contenga el HTML que describe el detalle del evento
 
--- js/registro.js
+++ js/registro.js
@@ -1,7 +1,58 @@
 // Hemos omitido los acentos en los comentarios por compatibilidad
-
+var valida;
 function validar(formulario) {
+  if (formulario.nombres.value.trim().length == "") {
+    document.getElementById('errornombres').innerHTML = "Este campo es obligatorio"
+    valida = false;
+  }else{
+    document.getElementById('errornombres').innerHTML = "";
+    valida = true;
+  }
 
-  //Expresion regular del correo
 
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(formulario.email.value)) {
+    document.getElementById("errorEmail").innerHTML = "Campo invalido";
+    valida = false;
+  }else{
+    document.getElementById("errorEmail").innerHTML = "";
+    valida = true;
+  }
+
+  if(formulario.contrasena.value.trim().length < 7){
+    document.getElementById("errorContrasena").innerHTML = "La contraseña debe tener minimo 7 caracteres";
+    formulario.contrasena.focus();
+    valida = false;
+  }else{
+    document.getElementById("errorContrasena").innerHTML = "";
+    valida = true;
+  }
+  if(formulario.contrasena.value != formulario.confirmacion.value || formulario.confirmacion.value == ""){
+    document.getElementById("errorConfirmacion").innerHTML = "Las contraseñas no coinciden";
+    valida = false;
+  }else{
+      document.getElementById("errorConfirmacion").innerHTML = "";
+      valida = true;
+  }
+  if (formulario.tipo.value < 0) {
+    document.getElementById("errorTipo").innerHTML = "Este campo es obligatorio";
+    valida = false;
+  }else{
+      document.getElementById("errorTipo").innerHTML = "";
+      valida = true;
+  }
+  if (!formulario.acepto.checked) {
+    document.getElementById("errorAcepto").innerHTML = "Este campo es obligatorio";
+    valida = false;
+  }else{
+    document.getElementById("errorAcepto").innerHTML = "";
+    valida = true;
+  }
+
+  if(valida == false){
+    return false;
+  }else{
+    alert ("¡Registro exitoso!")
+    return true;
+  }
 }
